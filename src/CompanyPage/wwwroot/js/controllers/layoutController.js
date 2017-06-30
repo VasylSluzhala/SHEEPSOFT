c25e204eda253ba44f1409285cf28b7373d96439
Vasyl0904 <mu.1.chested.09@gmail.com>
2017-06-30 20:13:27 +0300

    fix css couldn't upload, google maps

--- src/CompanyPage/wwwroot/js/controllers/layoutController.js
+++ src/CompanyPage/wwwroot/js/controllers/layoutController.js
@@ -1,7 +1,7 @@
 ﻿
 function redirectTo(loc) {
     var path = window.location.pathname;
-    if (path.indexOf("Partnership") != -1 || path.indexOf("Portfolio") != -1) {
+    if (path.indexOf("Partnership") !== -1 || path.indexOf("Portfolio") !== -1) {
         window.location.href = "/" + loc;
     }
 }
