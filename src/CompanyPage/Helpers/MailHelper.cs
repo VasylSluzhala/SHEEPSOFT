c4b379507040de3bd04ea0f3f144eafd5685bba4
DarkZett <schepanskiy1994@gmail.com>
2017-07-11 17:40:47 +0300

    Added missed files

---
+++ src/CompanyPage/Helpers/MailHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Http;
+using MimeKit;
+
+namespace CompanyPage.Helpers
+{
+    public class MailHelper
+    {
+        public static void Send(string mailText, string subject, List<string> mails)
+        {
+            foreach (var mail in mails)
+            {
+                Send(mailText, subject, mail);
+            }
+        }
+        public static void Send(string mailText, string subject, string mail)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("vasandrosmail@gmail.com"));
+            message.To.Add(new MailboxAddress(mail));
+            message.Subject = subject;
+            message.Body = new TextPart("plain")
+            {
+                Text = mailText
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587);
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                // Note: since we don't have an OAuth2 token, disable 	// the XOAUTH2 authentication mechanism.     
+                client.Authenticate("vasandrosmail@gmail.com", "qwerty1111");
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+    }
+}
---
+++ src/CompanyPage/ViewModel/ContactUsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyPage.ViewModel
+{
+    public class ContactUsViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
