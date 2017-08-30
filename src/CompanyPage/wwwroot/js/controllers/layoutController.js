55e95296ff80591a25798da140709dd831c411a0
Vasyl0904 <mu.1.chested.09@gmail.com>
2017-08-30 14:29:27 +0300

    optimize loading speed, animation speedup

--- src/CompanyPage/wwwroot/js/controllers/layoutController.js
+++ src/CompanyPage/wwwroot/js/controllers/layoutController.js
@@ -2,3 +2,11 @@
 $(document).on('click', function () {
     $('.navbar-collapse').collapse('hide');
 })
+
+function set_body_height() { // set body height = window height
+    $('#headercont').height($(window).height());
+}
+$(document).ready(function () {
+    $(window).bind('resize', set_body_height);
+    set_body_height();
+});
